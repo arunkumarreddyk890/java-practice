8a1fa04b5d0fc3f97b38df0cdf162a6414c99ae4
Kancham arun kumar reddy <arunkumarreddyk890@gmail.com>
2025-07-28 20:18:54 +0530

    initial commit

---
+++ sample programs/patterns/Diamond.java
@@ -0,0 +1,56 @@
+import java.util.*;
+class Diamond
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		int i,j,row;
+		System.out.print("Enter no.of rows : ");
+		row=sc.nextInt();
+		
+		for(i=1;i<=row;i++)
+		{
+			for(j=1;j<=row-i;j++)
+			{
+				System.out.print(" ");
+			}
+			//space--;
+			for(j=1;j<=i;j++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println("");
+		}
+		
+			int space=1;
+			for(i=1;i<=row-1;i++)
+			{
+				for(j=1;j<=space;j++)
+				{
+					System.out.print(" ");
+				}
+				space++;
+				for(j=1;j<=row-i;j++)
+				{
+					System.out.print("* ");
+				}
+				System.out.println("");
+			}
+	}
+}
+
+
+OUTPUT:-
+
+Enter no.of rows : 6
+     *
+    * *
+   * * *
+  * * * *
+ * * * * *
+* * * * * *
+ * * * * *
+  * * * *
+   * * *
+    * *
+     *
